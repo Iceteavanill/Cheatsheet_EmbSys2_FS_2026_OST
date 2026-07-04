2d6520cb93fb6a77bdba80566fa9b79ef738b9c2
Iceteavanill <Iceteavanill@users.noreply.github.com>
2026-07-04 17:42:21 +0200

    Improved example

--- code/Serialization_Parser_FlatBuffer.cpp
+++ code/Serialization_Parser_FlatBuffer.cpp
@@ -1,5 +1,9 @@
 // Serializer using flat buffer
-flatbuffers::FlatBufferBuilder builder;
-auto msg = CommunicationObjects::CreateDataMessage(builder, ts, temp);
-builder.Finish(msg);
-cp->Transmit(builder.GetBufferPointer(), builder.GetSize());
+flatbuffers::FlatBufferBuilder builder(64); // init builder object with 64b memory
+for (auto dp : data) { // loop through datapoints,
+    builder.Clear();
+    auto datapoint_offset = CommunicationObjects::CreateDataMessage(
+            builder, dp.timestamp, dp.temperature); // create flatbuffer object
+    builder.Finish(datapoint_offset);
+    cp->Transmit(builder.GetBufferPointer(), builder.GetSize());// send object
+}
